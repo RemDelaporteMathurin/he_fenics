3530dd0be430ec7433fb7004566b85b70eca9472
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-07-04 11:28:28 +0200

    better plot

--- sensitivity_dissociation_energies/plot.py
+++ sensitivity_dissociation_energies/plot.py
@@ -3,49 +3,63 @@ import matplotlib.pyplot as plt
 
 cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
+def plot_concentration(i, T):
+    
+    lines = []
+    for offset, style in zip([-0.5, 0, 0.5], ["-.", 'solid', 'dashed']):
+        folder = "{}K/{}eV".format(T, offset)
+        if i == "cb":
+            colour = "black"
+            data = np.genfromtxt(folder + "/profile_cb.csv", delimiter=",", names=True)
+            label = "$c_b$"
+            y = data[i]
+
+        else:
+            colour = cycle[i]
+            data = np.genfromtxt(folder + "/profiles.csv", delimiter=",", names=True)
+            label = "He" + str(i+1)
+            y = data[str(i+1)]
+
+        x = data["arc_length"]*1e9
+        
+        line, = plt.plot(x, y, linestyle=style, color=colour)
+        lines.append(y)
+        if offset == 0:
+            line.set_label(label)
+    
+    plt.fill_between(x, lines[0], lines[2], color=colour, alpha=0.2)
 
 def plot_profiles(T):
     for i in range(6):
-        lines = []
-        for offset, style in zip([-0.5, 0, 0.5], ["-.", 'solid', 'dashed']):
-            folder = "{}K/{}eV".format(T, offset)
-            data = np.genfromtxt(folder + "/profiles.csv", delimiter=",", names=True)
-            data_cb = np.genfromtxt(folder + "/profile_cb.csv", delimiter=",", names=True)
-            x = data["arc_length"]
-            y = data[str(i+1)]
-            line, = plt.plot(x, y, linestyle=style, color=cycle[i])
-            lines.append(y)
-            if offset == 0:
-                line.set_label("He" + str(i+1))
-        plt.fill_between(x, lines[0], lines[2], color=cycle[i], alpha=0.2)
+        plot_concentration(i, T)
 
     # plot cb
-    lines = []
-    for offset, style in zip([-0.5, 0, 0.5], ["-.", 'solid', 'dashed']):
-
-        folder = "{}K/{}eV".format(T, offset)
-        data_cb = np.genfromtxt(folder + "/profile_cb.csv", delimiter=",", names=True)
-        x = data["arc_length"]
-
-        line, = plt.plot(data_cb["arc_length"], data_cb["cb"], color="black", linestyle=style, linewidth=1)
-        lines.append(data_cb["cb"])
-        if offset == 0:
-            line.set_label("$C_b$")
-    plt.fill_between(x, lines[0], lines[2], color="black", alpha=0.2)
+    plot_concentration("cb", T)
 
 
-fig, axs = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(8, 3))
-for ax, T in zip(axs, [500, 1000, 1500]):
-    plt.sca(ax)
-    plot_profiles(T)
-    plt.xlabel("x (m)")
-    plt.text(0, 1e23, "{} K".format(T))
-axs[0].set_ylabel("Concentration (m$^{-3}$)")
-plt.ylim(bottom=1e6, top=5e24)
+fig, axs = plt.subplots(7, 3, sharex=True, sharey="row", figsize=(8, 3*3))
+for axs_row, i in zip(axs, [0, 1, 2, 3, 4, 5, "cb"]):
+    if i != "cb":
+        label = "$c_{\mathrm{He}_"+  "{}".format(i+1) + "}$"
+        if i==0:
+            label += " (m$^{-3}$)"
+        axs_row[0].set_ylabel(label)
+    else:
+        axs_row[0].set_ylabel("$c_b$")
+    
+    for ax, T in zip(axs_row, [500, 1000, 1500]):
+        plt.sca(ax)
+        plot_concentration(i, T)
+        plt.yscale("log")
+        ax.spines.right.set_visible(False)
+        ax.spines.top.set_visible(False)
+for ax in axs[-1]:
+    ax.set_xlabel("x (nm)")
 
-fig.suptitle("Solid: +0eV ; Dashed: +0.5eV ; Dashed-point: -0.5eV")
-plt.yscale("log")
-axs[-1].legend(bbox_to_anchor=(1.5, 0.5), loc="center right")
-# axs[0].legend(loc="lower center", ncol=4)
+# fig.suptitle("Solid: +0eV ; Dashed: +0.5eV ; Dashed-point: -0.5eV")
+
+axs[0][0].set_title("500 K")
+axs[0][1].set_title("1000 K")
+axs[0][2].set_title("1500 K")
 plt.tight_layout()
 plt.show()
